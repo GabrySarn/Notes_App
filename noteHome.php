7bd64f76436f14dc8ae3f40745ae066a558c4637
GabrySarn <125447671+GabrySarn@users.noreply.github.com>
2024-01-29 10:08:15 +0100

    scd

--- noteHome.php
+++ noteHome.php
@@ -189,7 +189,7 @@
     <header>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Notes App</a>
+            <a class="navbar-brand" href="#">Notes ciao</a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item dropdown">
