2637a676cccb7c3caec6cb862e701813fde3da98
GabrySarn <gabrics18022005@gmail.com>
2024-01-31 13:54:49 +0100

    tt

--- noteHome.php
+++ noteHome.php
@@ -465,22 +465,6 @@
 
     </div>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            // Ottieni il parametro folder_id dalla query string dell'URL
-            const urlParams = new URLSearchParams(window.location.search);
-            const selectedFolderId = urlParams.get('folder_id');
-
-            // Seleziona dinamicamente la cartella nel menu a discesa
-            if (selectedFolderId) {
-                const folderSelect = document.getElementById('cartella');
-                if (folderSelect) {
-                    folderSelect.value = selectedFolderId;
-                }
-            }
-        });
-    </script>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
